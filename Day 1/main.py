740ca222f4cb7c887e3994a668fda9deb0d180ef
TomWatson6 <37339340+TomWatson6@users.noreply.github.com>
2021-12-01 13:34:36 +0000

    Cleaned up the code for Day 1 both parts

--- Day 1/main.py
+++ Day 1/main.py
@@ -1,26 +1,26 @@
+def num_increasing(array):
+    increasing = 0
+    
+    for x in range(1, len(array), 1):
+        if array[x] > array[x - 1]:
+            increasing += 1
 
+    return increasing
 
 f = open("input.txt")
 depths = [int(x.strip("\n")) for x in f.readlines()]
 f.close()
 
-num_increased = 0
-
-for x in range(1, len(depths), 1):
-    if depths[x] > depths[x - 1]:
-        num_increased += 1
+num_increased = num_increasing(depths)
 
 print("Part 1:", num_increased)
 
-num_increased = 0
 triplets = []
 
 for x in range(1, len(depths) - 1, 1):
     sum = depths[x - 1] + depths[x] + depths[x + 1]
     triplets.append(sum)
 
-for x in range(1, len(triplets), 1):
-    if triplets[x] > triplets[x - 1]:
-        num_increased += 1
+num_increased = num_increasing(triplets)
 
 print("Part 2:", num_increased)
