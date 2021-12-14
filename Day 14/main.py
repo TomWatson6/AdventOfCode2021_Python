bcf2e7e33ebad0f24363a3db0205a37905595767
TomWatson6 <37339340+TomWatson6@users.noreply.github.com>
2021-12-14 13:59:13 +0000

    Cleaned up some naming conventions, and removed debug line

--- Day 14/main.py
+++ Day 14/main.py
@@ -1,10 +1,10 @@
 
 from collections import defaultdict
 
-def p1out(output):
+def p1(output):
     print("Part 1:", output)
 
-def p2out(output):
+def p2(output):
     print("Part 2:", output)
 
 with open("input.txt") as f:
@@ -21,13 +21,13 @@ rules = dict()
 for line in lines:
     rules[line[0]] = line[1]
 
-p1 = defaultdict(int)
-p2 = defaultdict(int)
+p1_pairs = defaultdict(int)
+p2_pairs = defaultdict(int)
 
 for x in range(len(polymer) - 1):
     pair = polymer[x:x + 2]
-    p1[pair] += 1 
-    p2[pair] += 1
+    p1_pairs[pair] += 1 
+    p2_pairs[pair] += 1
 
 def evolve(pairs, rules):
     new_pairs = defaultdict(int)
@@ -58,24 +58,22 @@ steps = 40
 
 for step in range(steps):
     if step < 10:
-        p1 = evolve(p1, rules)
+        p1_pairs = evolve(p1_pairs, rules)
     
-    p2 = evolve(p2, rules)
+    p2_pairs = evolve(p2_pairs, rules)
 
-totals1 = count_letters(p1)
+totals1 = count_letters(p1_pairs)
 
 totals1 = [x for x in totals1.values()]
 totals1.sort()
 
-print(totals1)
+p1((totals1[-1] - totals1[0]) // 2)
 
-p1out((totals1[-1] - totals1[0]) // 2)
-
-totals2 = count_letters(p2)
+totals2 = count_letters(p2_pairs)
 
 totals2 = [x for x in totals2.values()]
 totals2.sort()
 
-p2out((totals2[-1] - totals2[0]) // 2)
+p2((totals2[-1] - totals2[0]) // 2)
 
 
